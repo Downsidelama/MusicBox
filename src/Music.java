cb0214779734783cc4918da5f0b1d264c757a042
Downsidelama <27288245+Downsidelama@users.noreply.github.com>
2019-04-16 18:13:10 +0200

    Implementing song and lyrics adding

--- src/Music.java
+++ src/Music.java
@@ -2,16 +2,37 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Music {
-	String title;
-	List<Pair<String, String>> notes;
-	
-	public Music(String title) {
-		this.title = title;
-		this.notes = new ArrayList<>();
-	}
-	
-	public void addNote(String note, String length) {
-		this.notes.add(new Pair<String, String>(note, length));
-		
-	}
+    private final List<Pair<String, String>> notes;
+    private String lyrics = "";
+
+    Music() {
+        this.notes = new ArrayList<>();
+    }
+
+    void addNote(String note, String length) {
+        synchronized (this.notes) {
+            this.notes.add(new Pair<>(note, length));
+        }
+    }
+
+    void addLyrics(String lyrics) {
+        synchronized (this) {
+            this.lyrics = lyrics;
+        }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder stringOfMusic = new StringBuilder("Notes: ");
+        for (Pair<String, String> note : notes) {
+            stringOfMusic.append(note.getP1()).append(" ").append(note.getP2()).append(" ");
+        }
+        stringOfMusic = new StringBuilder(stringOfMusic.toString().trim());
+        stringOfMusic.append(",\r\nLyrics: ").append(lyrics);
+        return stringOfMusic.toString();
+    }
+
+    void clearNotes() {
+        this.notes.clear();
+    }
 }
--- src/MusicBox.java
+++ src/MusicBox.java
@@ -1,47 +1,111 @@
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 public class MusicBox {
 
-	List<ClientHandler> clients;
-	
-	public static void main(String... strings) {
-		new MusicBox();
-	}
+    private Map<String, Music> musicMap;
+    private List<ClientHandler> clients;
+    private ExecutorService executorService;
 
-	public MusicBox() {
-		clients = new ArrayList<>();
-		try (ServerSocket ss = new ServerSocket(40000)) {
-			while (true) {
-				acceptNewClient(ss);
-			}
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-	}
+    public static void main(String... strings) {
+        new MusicBox();
+    }
 
-	private void acceptNewClient(ServerSocket ss) throws IOException {
-		ClientHandler musicClient = new ClientHandler(ss.accept());
-		clients.add(musicClient);
-		Thread t = new Thread(musicClient);
-		t.start();
-	}
+    public MusicBox() {
+        musicMap = new HashMap<>();
+        executorService = Executors.newCachedThreadPool();
+        clients = new ArrayList<>();
+        try (ServerSocket ss = new ServerSocket(40000)) {
+            for (; ; ) {
+                acceptNewClient(ss);
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void acceptNewClient(ServerSocket ss) throws IOException {
+        ClientHandler musicClient = new ClientHandler(ss.accept(), musicMap);
+        clients.add(musicClient);
+        this.executorService.submit(musicClient);
+    }
 }
 
-class ClientHandler implements Runnable {
+class ClientHandler implements Runnable, AutoCloseable {
 
-	Socket s;
+    private Socket socket;
+    private Scanner input;
+    private PrintWriter output;
+    private boolean running;
+    private Map<String, Music> musicMap;
 
-	public ClientHandler(Socket s) {
-		super();
-		this.s = s;
-	}
+    ClientHandler(Socket s, Map<String, Music> musicMap) throws IOException {
+        super();
+        this.musicMap = musicMap;
+        this.socket = s;
+        this.input = new Scanner(s.getInputStream());
+        this.output = new PrintWriter(s.getOutputStream());
+        this.running = true;
+    }
 
-	@Override
-	public void run() {
+    @Override
+    public void run() {
+        while (this.running && this.input.hasNextLine()) {
+            String input = this.input.nextLine();
 
-	}
+            if (input.startsWith("addlyrics")) {
+                String title = input.replace("addlyrics", "").trim();
+                String lyrics = this.input.nextLine();
+                addLyrics(title, lyrics);
+            } else if (input.startsWith("add")) {
+                String title = input.replace("add", "").trim();
+                String notes = this.input.nextLine();
+                addMusic(title, notes);
+            } else {
+                System.out.println(input);
+            }
+        }
+        try {
+            this.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void addMusic(String title, String notes) {
+        Music music = this.musicMap.getOrDefault(title, new Music());
+        String[] splittedNotes = notes.split(" ");
+        if (splittedNotes.length % 2 == 0) {
+            music.clearNotes();
+            for (int i = 0; i < splittedNotes.length; i += 2) {
+                music.addNote(splittedNotes[i], splittedNotes[i + 1]);
+            }
+            System.out.println(music);
+            this.musicMap.put(title, music);
+        } else {
+            System.out.println("Bad notes format!");
+        }
+    }
+
+    private void addLyrics(String title, String lyrics) {
+        Music music = this.musicMap.getOrDefault(title, new Music());
+        music.addLyrics(lyrics);
+    }
+
+    @Override
+    public void close() throws Exception {
+        this.input.close();
+        this.output.close();
+        this.socket.close();
+    }
+
+    void dropClient() {
+        this.running = false;
+    }
 }
